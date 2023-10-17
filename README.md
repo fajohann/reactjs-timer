cffca0775feb9d9bf0515be6e3450f6d0ab91f61
Flavio Johann <flajohann@gmail.com>
2023-10-17 14:28:42 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align="center">
+  <img alt="timer" title="Timer" src=".github/logo-ignite.svg" />
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-executar">Como executar</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="https://reactjs-timer.vercel.app/" target="_blank">Exemplo</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="timer" src=".github/img-app.png" width="100%">
+  <img alt="historico" src=".github/img-app-2.png" width="100%">
+</p>
+
+## ✨ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://reactjs.org)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vite](https://vitejs.dev/)
+
+## 💻 Projeto
+
+Exemplo de aplicação no estilo **timer**, que contém as seguintes funcionalidades:
+
+- Adicionar um novo timer
+- Interromper um timer ativo
+- Histórico de timers
+
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Instale as dependências com `npm install`
+- Inicie o servidor com `npm run dev`
+
+Agora você pode acessar [`localhost:5173`](http://localhost:5173) do seu navegador.
+
+
+## 📄 Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by Flávio Johann
---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,105 @@
+import { ReactNode, createContext, useEffect, useReducer, useState } from "react";
+import { Cycle, CyclesReducer } from "../reducers/cycles/reducer";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
+
+interface CreateCycleData {
+    task: string
+    minutesAmount: number
+}
+
+interface CyclesContextType {
+    cycles: Cycle[]
+    activeCycle: Cycle | undefined
+    activeCycleId: string | null   
+    amountSecondsPassed: number
+    markCurrentCycleAsFinished: () => void
+    setSecondsPassed: (seconds:number) => void
+    createNewCycle: (data:CreateCycleData) => void
+    interruptCurrentCycle: () => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextType)
+
+interface CyclesContextProviderProps {
+    children: ReactNode
+}
+
+export function CyclesContextProvider({ children }:CyclesContextProviderProps) {
+    
+    const [cyclesState,dispatch] = useReducer(
+        CyclesReducer,
+        { 
+            cycles:[], 
+            activeCycleId: null, 
+        }, 
+        (initialState) => {
+
+            const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state')
+
+            if (storedStateAsJSON) {
+                return JSON.parse(storedStateAsJSON)
+            }
+
+            return initialState
+        })
+
+    const { cycles, activeCycleId } = cyclesState
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+    const [amountSecondsPassed,setAmountSecondsPassed] = useState(() => {
+        if (activeCycle) {
+            return differenceInSeconds(
+                new Date(),
+                new Date(activeCycle.startDate),
+            )
+        }
+        return 0
+    })
+
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState)
+        localStorage.setItem('@ignite-timer:cycles-state',stateJSON)
+    },[cyclesState])
+
+    function markCurrentCycleAsFinished() {        
+        dispatch(markCurrentCycleAsFinishedAction()) 
+    }
+
+    function setSecondsPassed(seconds:number) {
+        setAmountSecondsPassed(seconds)
+    }
+
+    function createNewCycle(data:CreateCycleData) {
+        const id = String(new Date().getTime())
+
+        const newCycle:Cycle = {
+            id: id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+            startDate: new Date()
+        }
+        dispatch(addNewCycleAction(newCycle))
+        setAmountSecondsPassed(0)
+    }
+
+    function interruptCurrentCycle() {        
+        dispatch(interruptCurrentCycleAction())       
+    }
+
+    return (
+        <CyclesContext.Provider value={{ 
+                cycles,
+                activeCycle,
+                activeCycleId,    
+                amountSecondsPassed,
+                markCurrentCycleAsFinished,
+                setSecondsPassed,
+                createNewCycle,
+                interruptCurrentCycle,
+            }}
+        >
+            {children}
+        </CyclesContext.Provider>
+    )
+}
